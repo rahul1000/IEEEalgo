c5c9bf674c93febf9286ca5a88dc294982356740
rahul1000 <rahul-100@hotmail.com>
2015-05-10 23:17:17 +0530

    This is a test upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+
+{int x,y;
+cout<<"\nenter the two numbers:";
+cin>>x>>y;
+
+    cout << "Hello world!" << endl;
+   cout<<"\nsum of two numbers is:"<<(x+y);
+    return 0;
+    getche();
+}
